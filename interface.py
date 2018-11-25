6ff4c068726ed9ef2c18b0dbc3d923b99fbd04e3
Luis Humberto Orozco Cabrera <humbledore@gmail.com>
2018-11-25 10:49:44 -0500

    Distribute code

--- interface.py
+++ interface.py
@@ -4,193 +4,16 @@
 
 import sys
 
-from helpers import *
-from errors import *
+from utils.helpers import APP_TITLE, APP_OPTIONS, APP_FINISHED
+from utils.errors import INVALID_CHOICE
+from utils.process_expression import make_list, calculate
+from utils.validation import check_valid_values, check_surroundings_VALID_OPERATORS, check_first_last_values, check_no_empty_parenthesis, check_equal_open_close_parenthesis, check_surroundings_and_order_parenthesis
 
 def banner(text, icon_upper=None, icon_lower=None):
     '''Makes nice signs with text'''
     row_upper = icon_upper * (len(text) + 6) + "\n" if icon_upper else ""
     row_lower = "   \n" + icon_lower * (len(text) + 6) if icon_lower else ""
     print(row_upper+ "   " + text + row_lower)
-
-def check_valid_values(instructions):
-    '''Checks that the expresion contains just valid values'''
-    for char_index in range(len(instructions)):
-        if instructions[char_index] not in VALID_OPERATORS+VALID_DIGITS+PARENTHESIS_SIMBOLS:
-            print(INVALID_SYMBOL_OR_VALUE)   
-            return False
-    return True
-           
-def check_surroundings_VALID_OPERATORS(instructions):
-    '''Checks for operators (+-:/*) not to be one next to the other'''
-    for char_index in range(len(instructions)):
-        if instructions[char_index] in VALID_OPERATORS:
-            if instructions[char_index - 1] in VALID_OPERATORS or instructions[char_index + 1] in VALID_OPERATORS:   
-                print(OPERATORS_TOGETHER)
-                return False
-    return True
-
-def check_first_last_values(instructions):
-    '''CHecks the first and last values to be either digit or parenthesis sign'''
-    if instructions[0] not in VALID_DIGITS+PARENTHESIS_SIMBOLS or instructions[-1] not in VALID_DIGITS+PARENTHESIS_SIMBOLS:
-        print(FIRST_LAST_VALUES_PAR)
-        return False
-    return True
-
-def check_no_empty_parenthesis(instructions):
-    '''Looks for empty parenthesis: ()'''
-    if "()" in instructions:
-        print(EMPTY_PARENTHESIS)
-        return False
-    return True
-
-def check_equal_open_close_parenthesis(instructions):
-    '''Checks for equal amount of open '(' and close ')' parenthesis'''
-    qty_open_parenthesis = instructions.count('(')
-    qty_closing_parenthesis = instructions.count(')')
-    if qty_open_parenthesis != qty_closing_parenthesis:
-        print(EXTRA_OR_MISSING_PAR)
-        return False
-    return True
-
-def check_surroundings_and_order_parenthesis(instructions):
-    ''' Checks for next scenarios:
-    - Order of parenthesis: ')' before '(' .
-    - Non-sense values next to parenthesis:
-        - '(' :
-            - Before: '0(', '1(', '2(', '3(', '4(', '5(', '6(', '7(', '8(', '9('
-            - After: '(+', '(-', '(:', '(*', '(/'
-        - ')' :
-            - Before: '+)', '-)', ':)', '*)', '/)'
-            - After: ')0', ')1', ')2', ')3', ')4', ')5', ')6', ')7', ')8', ')9'
-      '''
-    count_for_order = 0
-    for char_index in range(len(instructions)):
-        if instructions[char_index] == ")" :
-            count_for_order -= 1
-        elif instructions[char_index] == "(" :
-            count_for_order += 1
-        if count_for_order < 0:
-            print(WRONG_ORDER_PARENTHESIS)
-            return False
-
-        if instructions[char_index] == "(":
-            if instructions[char_index+1] in VALID_OPERATORS:
-                print(INVALID_VALUE_RIGHT_OPEN_PAR)
-                return False
-            if char_index != 0:
-                if instructions[char_index-1] in VALID_DIGITS:
-                    print(INVALID_VALUE_LEFT_OPEN_PAR)
-                    return False
-
-        elif instructions[char_index] == ")":
-            if instructions[char_index-1] in VALID_OPERATORS:
-                print(INVALID_VALUE_LEFT_CLOSING_PAR)
-                return False
-            elif char_index != len(instructions) - 1:
-                if instructions[char_index+1] in VALID_DIGITS:
-                    print(INVALID_VALUE_RIGHT_CLOSING_PAR)
-                    return False
-    return True
-            
-def make_list(instructions):
-    ''' Makes list with independant terms of entered expression '''
-    list_calculus = []
-    index = 0
-    for char_index in range(len(instructions)):
-        print("char_index: {}, value: {}".format(char_index, instructions[char_index]))
-        if instructions[char_index] in VALID_OPERATORS+PARENTHESIS_SIMBOLS:
-            if instructions[index:char_index] != '':
-                list_calculus.append(int(instructions[index:char_index]))
-            list_calculus.append(instructions[char_index])    # OK
-            index = char_index + 1
-        # LAST NUMBER
-        if char_index == len(instructions) - 1 :
-            if instructions[index:len(instructions)] != '':
-                list_calculus.append(int(instructions[index:len(instructions)]))
-    print("\nList_terms: "+str(list_calculus)+"\n")
-    return list_calculus
-
-def make_operation(number1, number2, operator):
-    ''' Performs a single operation between two numbers '''
-    if operator == ":" or operator == "/" :
-        if number2 != 0 :
-            reduced_member = number1/number2  
-        else: 
-            print(TROLL_FACE)
-            print(DIVISION_BY_ZERO)
-            menu()
-    elif operator == "*" :
-        reduced_member = number1*number2
-    elif operator == "+" :
-        reduced_member = number1+number2
-    else:                                   # operator == "-" 
-        reduced_member = number1-number2
-    return reduced_member
-
-# EXAMPLE : [23, '+', 34, '-', 98, ':', 29, '/', 34, '/', 98]
-def reduce_members(list_calculus):
-    ''' Prioritize order of operations 
-        *, :, / in order that appears before +, -
-    '''
-    while True:
-        if "*" in list_calculus or ":" in list_calculus or "/" in list_calculus:
-            for operator_index in range(1,len(list_calculus),2):
-                print("Operator index - 1: {}, value: {}".format(operator_index-1, list_calculus[operator_index-1]))
-                print("Operator index: {}, value: {}".format(operator_index, list_calculus[operator_index]))
-                print("Operator index + 1: {}, value: {}".format(operator_index+1, list_calculus[operator_index+1]))
-                if list_calculus[operator_index] in PRIORITY_OPERATORS :
-                    temp = make_operation(list_calculus[operator_index-1],list_calculus[operator_index+1],list_calculus[operator_index])
-                    list_calculus[operator_index-1] = temp
-                    del list_calculus[operator_index:operator_index+1+1] # Plus extra, sublist structure: [initial:final)
-                    print("Reduced_member: "+str(list_calculus)+"\n")
-                    break
-                else:
-                    print(PRIORITY_MESSAGE)
-                    pass
-        else:
-            for operator_index in range(1,len(list_calculus),2):
-                print("Operator index - 1: {}, value: {}".format(operator_index-1, list_calculus[operator_index-1]))
-                print("Operator index: {}, value: {}".format(operator_index, list_calculus[operator_index]))
-                print("Operator index + 1: {}, value: {}".format(operator_index+1, list_calculus[operator_index+1]))
-                temp = make_operation(list_calculus[operator_index-1],list_calculus[operator_index+1],list_calculus[operator_index])
-                list_calculus[operator_index-1] = temp
-                del list_calculus[operator_index:operator_index+1+1] # Plus extra, sublist structure: [initial:final)
-                print("Reduced_member: "+str(list_calculus)+"\n")
-                break
-        if len(list_calculus) == 1:
-            return list_calculus[0]
-        else:
-            pass
-        
-def calculate(main_list):
-    ''' Reduce parenthesis expressions and final expression '''
-    while True:
-        start_parenthesis = 0
-        end_parenthesis = 0
-        for index in range(len(main_list)):
-            if '(' in main_list:
-                if main_list[index] == "(":
-                    start_parenthesis = index
-                if main_list[index] == ")":
-                    end_parenthesis = index
-                if end_parenthesis:
-                    print("Parenthesis: {}".format(main_list[start_parenthesis+1:end_parenthesis]))
-                    temp = reduce_members(main_list[start_parenthesis+1:end_parenthesis])
-                    main_list[start_parenthesis] = temp
-                    del main_list[start_parenthesis+1:end_parenthesis+1] # Plus extra, sublist structure: [initial:final)
-                    print("Main_remaining_list_terms: "+str(main_list)+"\n") # h is just iindicator to identify when there was a parenthesis
-                    break
-            else:
-                main_list = reduce_members(main_list)
-                print("Final exact result: "+str(main_list)) # This is supposed to happen when there is just one term
-                print("Type final result: {}\n".format(str(type(main_list)))) 
-                if str(type(main_list)) == "<class 'int'>":
-                    return main_list
-                elif str(type(main_list)) == "<class 'float'>":
-                    return round(main_list, 2)
-                else:
-                    pass
 
 def menu():
     '''User interface'''
@@ -229,7 +52,6 @@ def menu():
                     break
                 
                 calculus = make_list(math_instructions)
-                
                 result=calculate(calculus)
 
                 print("Results: {}".format(result))
